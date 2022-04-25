c8403de7acdbca14e99178cc92488cb4e0939e94
Dave Caruso <dave@davecode.net>
2022-04-25 07:36:05 -0400

    it builds!

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,22 +1,41 @@
 import { PermissionFlagsBits } from 'discord-api-types/v10';
-import 'dotenv/config';
 
-function envNonEmptyString(name: string) {
-	const value = process.env[name];
+function envNonEmptyString(name: string, value: string) {
 	if (!value) {
 		throw new Error(`${name} environment variable is required`);
 	}
 	return value;
 }
 
-export const HOST = envNonEmptyString('HOST');
-export const DISCORD_API_URL = envNonEmptyString('DISCORD_API_URL');
-export const DISCORD_CLIENT_ID = envNonEmptyString('DISCORD_CLIENT_ID');
-export const DISCORD_CLIENT_SECRET = envNonEmptyString('DISCORD_CLIENT_SECRET');
-export const DISCORD_BOT_TOKEN = envNonEmptyString('DISCORD_BOT_TOKEN');
-export const DISCORD_PUBLIC_KEY = envNonEmptyString('DISCORD_PUBLIC_KEY');
+function envStringArray(name: string, value: string) {
+	if (!value) {
+		return [];
+	} else {
+		return value.split(',').map((s) => s.trim());
+	}
+}
+
+export const HOST = envNonEmptyString('HOST', process.env.HOST);
+export const DISCORD_API_URL = envNonEmptyString('DISCORD_API_URL', process.env.DISCORD_API_URL);
+export const DISCORD_CLIENT_ID = envNonEmptyString(
+	'DISCORD_CLIENT_ID',
+	process.env.DISCORD_CLIENT_ID
+);
+export const DISCORD_CLIENT_SECRET = envNonEmptyString(
+	'DISCORD_CLIENT_SECRET',
+	process.env.DISCORD_CLIENT_SECRET
+);
+export const DISCORD_BOT_TOKEN = envNonEmptyString(
+	'DISCORD_BOT_TOKEN',
+	process.env.DISCORD_BOT_TOKEN
+);
+export const DISCORD_PUBLIC_KEY = envNonEmptyString(
+	'DISCORD_PUBLIC_KEY',
+	process.env.DISCORD_PUBLIC_KEY
+);
+
 export const DISCORD_REDIRECT_URI = `${HOST}/api/callback`;
 
-export const SCOPES = ['identify', 'email', 'guilds'];
+export const SCOPES = ['identify', 'guilds'];
 export const SCOPES_BOT = [...SCOPES, 'bot', 'applications.commands'];
 export const PERMISSIONS = PermissionFlagsBits.ManageRoles + PermissionFlagsBits.SendMessages;
--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,10 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapterCloudflare from '@sveltejs/adapter-cloudflare';
 import preprocess from 'svelte-preprocess';
 import 'dotenv/config';
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,12 +13,38 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter(),
-		vite: {
-			define: {
-				'process.env.DATABASE_URL': JSON.stringify(process.env.DATABASE_URL)
-			}
-		}
+		adapter: adapterCloudflare({
+			define: Object.fromEntries(
+				Object.entries(process.env)
+					.filter(([key]) => !key.includes('('))
+					.map(([key, value]) => [`process.env.${key}`, JSON.stringify(value)])
+					.concat([
+						['process.env.NODE_ENV', JSON.stringify('production')],
+						['process.env', JSON.stringify({})]
+					])
+			),
+			plugins: [
+				{
+					name: 'node-awaitable-timers',
+					setup(build) {
+						build.onResolve({ filter: /timers\/promises/ }, () => ({
+							path: require.resolve('awaitable-timers')
+						}));
+					}
+				},
+				{
+					name: 'node-prefix',
+					setup(build) {
+						build.onResolve({ filter: /^node:.*$/ }, (args) => ({
+							path: args.path.slice(5),
+							namespace: 'node-modules-polyfills'
+						}));
+					}
+				},
+				NodeModulesPolyfillPlugin()
+			],
+			minify: true
+		})
 	}
 };
 
