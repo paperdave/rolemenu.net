e4ccac5cdfb6f1f10db7658ba0b0b009ca0022cf
Dave Caruso <dave@davecode.net>
2022-04-25 20:08:48 -0400

    meta owo

---
+++ src/lib/components/Meta.svelte
@@ -0,0 +1,14 @@
+<svelte:head>
+	<title>Role Menus for Discord</title>
+	<meta
+		name="description"
+		content="Role Menus are a more modern version of traditional “reaction roles” systems, allowing for self-assignable roles on Discord servers. Easily customize their appearance and functionality to your liking with our web-based editor."
+	/>
+	<meta name="og:title" content="Role Menus for Discord" />
+	<meta name="og:site_name" content="rolemenu.net" />
+	<meta
+		name="og:description"
+		content="Role Menus are a more modern version of traditional “reaction roles” systems, allowing for self-assignable roles on Discord servers. Easily customize their appearance and functionality to your liking with our web-based editor."
+	/>
+	<meta name="theme-color" content="#3d986c" />
+</svelte:head>
